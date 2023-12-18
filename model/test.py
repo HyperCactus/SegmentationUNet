8c709664e81fc8d19551881ac734a6ff33575ed1
HyperCactus <sampollard888@gmail.com>
2023-12-18 16:42:50 +1000

    HPC troubleshooting

--- model/test.py
+++ model/test.py
@@ -2,13 +2,12 @@ import torch
 
 print('Begin testing')
 print('torch version: ', torch.__version__)
-# # testing cuda
-# if torch.cuda.is_available():
-#     print('cuda is available')
-# else:
-#     print('cuda is NOT available')
+# testing cuda
+if torch.cuda.is_available():
+    print('cuda is available')
+else:
+    print('cuda is NOT available')
 
-# import torch
 # torch.zeros(1).cuda()
 
 print('Success')
