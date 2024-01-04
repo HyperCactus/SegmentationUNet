4a34b690b741116aac48641e90951f7d9ae1f1f7
HyperCactus <sampollard888@gmail.com>
2024-01-04 19:29:11 +1000

    HPC config

--- model/global_params.py
+++ model/global_params.py
@@ -7,7 +7,7 @@ HPC = True
 # Hyperparameters
 # LEARNING_RATE = 1e-4
 LEARNING_RATE = 1e-8
-BATCH_SIZE = 48 if HPC else 3
+BATCH_SIZE = 42 if HPC else 3
 # BATCH_SIZE = 3 # Laptop
 NUM_EPOCHS = 8 if HPC else 1
 NUM_WORKERS = 4
