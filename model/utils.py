3914a6bdbd866f9bf8a99ec1fdbe747ef1b002b4
HyperCactus <sampollard888@gmail.com>
2024-01-24 22:52:28 +1000

    testing branch

--- model/utils.py
+++ model/utils.py
@@ -944,4 +944,4 @@ def get_tile_nums(h, w, tile_size=TILE_SIZE, min_overlap=0.5):
     """
     tiles_in_x = 1 if w == tile_size else math.ceil(w / (tile_size * (1 - min_overlap)))
     tiles_in_y = 1 if h == tile_size else math.ceil(h / (tile_size * (1 - min_overlap)))
-    return tiles_in_x, tiles_in_y
+    return tiles_in_x, tiles_in_y# wtf
